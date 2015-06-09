3028370631fa764eecbd65258252b024f9df327e
Valentin Cardon <valentin.cardon@epitech.eu>
2015-06-09 19:58:10 +0200

    add flame.cpp pour les explosions

---
+++ class/Flame.cpp
@@ -0,0 +1,122 @@
+/*
+** Cube.cpp for Cube in /home/mathon_j/rendu/cpp_bomberman/class
+** 
+** Made by Jérémy MATHON
+** Login   <mathon_j@mathonj>
+** 
+** Started on  Wed May  6 15:57:46 2015 Jérémy MATHON
+// Last update Tue Jun  9 19:43:19 2015 Valentin Cardon
+*/
+
+#include	"Flame.hpp"
+
+Flame::Flame(int x, int y)
+{
+  _x = x;
+  _y = y;
+}
+
+Flame::~Flame()
+{
+
+}
+
+bool	Flame::initialize()
+{
+  // On charge la texture qui sera affichee sur chaque face du Cube
+  if (_texture.load("./lib/assets/flame.tga") == false)
+    {
+      std::cerr << "Cannot load the cube texture" << std::endl;
+      return (false);
+    }
+  // on set la color d'une premiere face
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+  // tout les pushVertex qui suivent seront de cette couleur
+  // On y push les vertices d une premiere face
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+  // Les UVs d'une premiere face
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+  
+  //Face de devant ou de derriere
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
+
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+
+  //face de droite ou de gauche
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+  
+  //face de droite ou de gauche
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
+
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+
+  //face de dessus
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+
+  //face de dessous
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
+  _geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+  _geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
+
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+
+  translate(glm::vec3(_x, 0, _y));
+  // Tres important, on n'oublie pas de build la geometrie pour envoyer ses informations a la carte graphique
+  _geometry.build();
+  return (true);
+}
+
+void Flame::update(gdl::Clock const &clock, gdl::Input &input)
+{}
+
+void Flame::draw(gdl::AShader &shader, gdl::Clock const &clock)
+{
+  (void)clock;
+  // On bind la texture pour dire que l'on veux l'utiliser
+  _texture.bind();
+  // Et on dessine notre cube
+  _geometry.draw(shader, getTransformation(), GL_QUADS);
+}
---
+++ header/Flame.hpp
@@ -0,0 +1,37 @@
+//
+// Flame.hpp for bomberman in /home/cardon_v/Documents/CPP/cpp_bomberman/header
+// 
+// Made by Valentin Cardon
+// Login   <cardon_v@epitech.net>
+// 
+// Started on  Tue Jun  9 19:40:37 2015 Valentin Cardon
+// Last update Tue Jun  9 19:41:18 2015 Valentin Cardon
+//
+
+#ifndef FLAME_HPP_
+# define FLAME_HPP_
+
+#include	<SdlContext.hh>
+#include	<glm/glm.hpp>
+#include	<glm/gtc/matrix_transform.hpp>
+#include	<Texture.hh>
+#include	<Geometry.hh>
+#include	"AObject.hpp"
+
+class Flame : public AObject
+{
+private:
+  gdl::Texture _texture;   // La texture utilisee pour le Cube
+  gdl::Geometry _geometry;   // La geometrie du Cube
+  int	_x;
+  int	_y;
+  
+public:
+  Flame(int, int);
+  virtual ~Flame();
+  bool	initialize();
+  void	update(gdl::Clock const &clock, gdl::Input &input);
+  void	draw(gdl::AShader &shader, gdl::Clock const &clock);
+};
+
+#endif /* !FLAME_HPP_ */
